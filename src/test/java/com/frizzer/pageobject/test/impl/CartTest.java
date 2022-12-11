8057f8b4aa61891b22583092579a17ccfbfb4e4a
GIGABYTE <eo.ursuz@gmail.com>
2022-12-11 23:45:12 +0300

    Multiple cart returned

--- src/test/java/com/frizzer/pageobject/test/impl/CartTest.java
+++ src/test/java/com/frizzer/pageobject/test/impl/CartTest.java
@@ -15,16 +15,16 @@ import java.util.List;
 public class CartTest extends CommonTest {
     @Test
     public void testCartByAddingMultipleElements(){
-//        driver.get("https://store.vaporesso.com/collections/collections");
-//        for (int i = 0; i < 2; i++) {
-//            ProductPage productPage = new ProductPage(driver);
-//            driver.get(productPage.findVapes().get(i).getLink());
-//            SingleProductPage singleProductPage = new SingleProductPage(driver).addToCart();
-//            driver.get("https://store.vaporesso.com/collections/collections");
-//        }
-//        driver.get("https://store.vaporesso.com/collections/collections");
-//        ProductPage productPage = new ProductPage(driver).openCart();
-//        Assert.assertEquals(productPage.countItemsInCart(),2);
+        driver.get("https://store.vaporesso.com/collections/collections");
+        for (int i = 0; i < 2; i++) {
+            ProductPage productPage = new ProductPage(driver);
+            driver.get(productPage.findVapes().get(i).getLink());
+            SingleProductPage singleProductPage = new SingleProductPage(driver).addToCart();
+            driver.get("https://store.vaporesso.com/collections/collections");
+        }
+        driver.get("https://store.vaporesso.com/collections/collections");
+        ProductPage productPage = new ProductPage(driver).openCart();
+        Assert.assertEquals(productPage.countItemsInCart(),2);
 
     }
 
